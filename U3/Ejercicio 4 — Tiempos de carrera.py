b0d0bc77a409f70f536afd3de04e26d3f2c7076d
jordanatrujeque1606-arch <jordanatrujeque.1606@gmail.com>
2026-08-04 08:56:44 -0600

    Ejercicio 4 — Tiempos de carrera.py
    Ejercicio 4 — Tiempos de carrera.py

---
+++ U3/Ejercicio 4 — Tiempos de carrera.py
@@ -0,0 +1,14 @@
+times = [58.2, 61.0, 55.4, 59.9, 57.1]
+
+print("Number of runners:", len(times))
+print("Fastest time:", min(times))
+print("Slowest time:", max(times))
+print("Sorted times:", sorted(times))
+print("Original list:", times)
+
+# Output:
+# Number of runners: 5
+# Fastest time: 55.4
+# Slowest time: 61.0
+# Sorted times: [55.4, 57.1, 58.2, 59.9, 61.0]
+# Original list: [58.2, 61.0, 55.4, 59.9, 57.1]
