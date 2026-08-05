040d7db675a61352cb5d8228630528aae7f54256
jordanatrujeque1606-arch <jordanatrujeque.1606@gmail.com>
2026-08-05 11:31:20 -0600

    Delete problem11.py

--- problem11.py
+++
@@ -1,8 +0,0 @@
-# Problema 11
-N = int(input("Ingresa un entero positivo N: "))
-i = 1          
-suma = 0       
-while i <= N:
-    suma = suma + i   
-    i = i + 1
-print("La suma total es:", suma)
