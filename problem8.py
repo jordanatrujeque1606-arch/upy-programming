87fde6d5fb7710472c8cc91e1f26e6eab2c19061
jordanatrujeque1606-arch <jordanatrujeque.1606@gmail.com>
2026-08-05 11:31:29 -0600

    Delete problem8.py

--- problem8.py
+++
@@ -1,9 +0,0 @@
-#problema8
-#leeunatemperaturaenCeeimprimeelestadodelagua
-temp = float(input("Ingresa la temperatura en grados Celsius: "))
-if temp <= 0:
-    print("estado:sólido(hielo)")
-elif 0 < temp < 100:
-    print("líquido(agua)")
-else:
-    print("gaseoso(vapor)")
